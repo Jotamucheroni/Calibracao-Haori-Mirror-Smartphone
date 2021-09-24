6e594ea128d23b12e953ecccd6b55bb06ca2317e
João Pedro Mucheroni Covolan <jotamucheroni@gmail.com>
2021-09-24 18:48:25 -0300

    RenderBuffer - reestruturação

--- app/src/main/java/com/unesp/calibracao_haori/opengl/RenderBuffer.java
+++ app/src/main/java/com/unesp/calibracao_haori/opengl/RenderBuffer.java
@@ -3,33 +3,45 @@ package com.unesp.calibracao_haori.opengl;
 import android.opengl.GLES32;
 
 public class RenderBuffer implements AutoCloseable {
-    public static final int numeroComponentesCor = 4; 
-    
-    private int largura, altura;
+    private static final int[] formatoInterno = new int[]{
+        GLES32.GL_R8, GLES32.GL_RG8, GLES32.GL_RGB8, GLES32.GL_RGBA8
+    };
     
     private final int id;
+    private final int largura, altura;
+    private final int numeroComponentesCor;
     
-    public RenderBuffer( int largura, int altura ) {
-        setLargura( largura );
-        setAltura( altura );
-        
+    public RenderBuffer( int largura, int altura, int numeroComponentesCor ) {
         int[] bufferId = new int[1];
         GLES32.glGenRenderbuffers( 1, bufferId, 0 );
         id = bufferId[0];
-    }
-    
-    public void setLargura( int largura ) {
+        
         if ( largura < 1 )
             largura = 1;
-        
         this.largura = largura;
-    }
-    
-    public void setAltura( int altura ) {
+        
         if ( altura < 1 )
             altura = 1;
-        
         this.altura = altura;
+        
+        if ( numeroComponentesCor < 1 )
+            numeroComponentesCor = 1;
+        else if ( numeroComponentesCor > 4 )
+            numeroComponentesCor = 4;
+        this.numeroComponentesCor = numeroComponentesCor;
+        
+        GLES32.glBindRenderbuffer( GLES32.GL_RENDERBUFFER, id );
+        GLES32.glRenderbufferStorage(
+            GLES32.GL_RENDERBUFFER, formatoInterno[this.numeroComponentesCor - 1], largura, altura
+        );
+    }
+    
+    public RenderBuffer( int largura, int altura ) {
+        this( largura, altura, 4 );
+    }
+    
+    public int getId() {
+        return id;
     }
     
     public int getLargura() {
@@ -40,29 +52,16 @@ public class RenderBuffer implements AutoCloseable {
         return altura;
     }
     
-    public int getId() {
-        return id;
+    public int getNumeroComponentesCor() {
+        return numeroComponentesCor;
     }
     
-    public int getNumPix() {
+    public int getNumeroPixeis() {
         return largura * altura;
     }
     
-    public int getNumBytes() {
-        return getNumPix() * RenderBuffer.numeroComponentesCor;
-    }
-    
-    private boolean alocado = false;
-    
-    public void alocar() {
-        GLES32.glBindRenderbuffer( GLES32.GL_RENDERBUFFER, id );
-        GLES32.glRenderbufferStorage( GLES32.GL_RENDERBUFFER, GLES32.GL_RGBA8, largura, altura );
-        
-        alocado = true;
-    }
-    
-    public boolean getAlocado() {
-        return alocado;
+    public int getNumeroBytes() {
+        return getNumeroPixeis() * numeroComponentesCor;
     }
     
     @Override
