93d1fa22d7f9c31843b20ff38998a1650b3486e0
João Pedro Mucheroni Covolan <jotamucheroni@gmail.com>
2021-10-26 19:52:48 -0300

    Correção do cálculo da projeção (Renderizador)

--- app/src/main/java/com/unesp/calibracao_haori/opengl/Renderizador.java
+++ app/src/main/java/com/unesp/calibracao_haori/opengl/Renderizador.java
@@ -47,24 +47,21 @@ public class Renderizador implements GLSurfaceView.Renderer, AutoCloseable {
         for ( int i = 0; i < NUMERO_PARAMETROS_TEXTURA; i++ )
             desenho.setParametroTextura( i, 0.1f );
         
+        float[] verticesQuadradoBranco = new float[] {
+            -1.0f,  1.0f,
+            -1.0f, -1.0f,
+             1.0f, -1.0f,
+             1.0f,  1.0f
+        };
+        
         quadradoCalibracao = new Desenho(
             2,
-            new float[] {
-                -1.0f,  1.0f,
-                -1.0f, -1.0f,
-                 1.0f, -1.0f,
-                 1.0f,  1.0f,
-            },
+            verticesQuadradoBranco,
             Desenho.getRefElementos()
         );
         quadradoTeste = new Desenho(
             2,
-            new float[] {
-                -1.0f,  1.0f,
-                -1.0f, -1.0f,
-                 1.0f, -1.0f,
-                 1.0f,  1.0f,
-            },
+            verticesQuadradoBranco,
             Desenho.getRefElementos()
         );
         
@@ -93,19 +90,17 @@ public class Renderizador implements GLSurfaceView.Renderer, AutoCloseable {
             float[] parametro = bluetooth.getParametrosQuadradoCalibracao();
             float
                 escalaX = parametro[2],
-                escalaY = parametro[3];
+                escalaY = parametro[3],
+                translacaoX = escalaX + ( parametro[0] - 1 ),
+                translacaoY = -escalaY + ( parametro[1] - 1 );
             
-            quadradoCalibracao.setEscala( escalaX, escalaY, 1 );
+            quadradoCalibracao.setEscala( escalaX, escalaY, 0 );
             quadradoCalibracao.setRotacao(
                 (float) Math.toRadians( parametro[4] - 90 ),
                 (float) Math.toRadians( parametro[5] - 90 ),
                 (float) Math.toRadians( parametro[6] - 90 )
             );
-            quadradoCalibracao.setTranslacao(
-                escalaX + ( parametro[0] - 1 ),
-                -escalaY + ( parametro[1] - 1 ),
-                0
-            );
+            quadradoCalibracao.setTranslacao( translacaoX, translacaoY, -1 );
             
             tela.draw( tela.getLargura() / 2, tela.getAltura(), quadradoCalibracao );
         }
@@ -113,12 +108,17 @@ public class Renderizador implements GLSurfaceView.Renderer, AutoCloseable {
         if ( bluetooth.exibirQuadradoTeste() ) {
             float[] parametro = bluetooth.getParametrosQuadradoTeste();
             
-            quadradoTeste.setProjecao( parametro[0], parametro[1] );
-            quadradoTeste.setTranslacaoTela( parametro[2] - 1, parametro[3] - 1 );
+            quadradoTeste.setEscala( parametro[7] - 1000, parametro[8] - 1000, 1 );
+            quadradoTeste.setTranslacao(
+                parametro[9] - 1000, parametro[10] - 1000, parametro[11] - 1000
+            );
+            
+            quadradoTeste.setProjecao( parametro[0] - 1000, parametro[1] - 1000 );
+            quadradoTeste.setTranslacaoTela( parametro[2] - 1000, parametro[3] - 1000 );
             quadradoTeste.setRotacaoTela(
-                (float) Math.toRadians( parametro[4] - 90 ),
-                (float) Math.toRadians( parametro[5] - 90 ),
-                (float) Math.toRadians( parametro[6] - 90 )
+                (float) Math.toRadians( parametro[4] - 90 - 1000 ),
+                (float) Math.toRadians( parametro[5] - 90 - 1000 ),
+                (float) Math.toRadians( parametro[6] - 90 - 1000 )
             );
             
             tela.draw( tela.getLargura() / 2, tela.getAltura(), quadradoTeste );
